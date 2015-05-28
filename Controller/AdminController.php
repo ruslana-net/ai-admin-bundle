1c8b4b37295d8702656e57a8fe4829adb0d748ae
Ruslan <info@aivad.ru>
2015-05-28 05:45:14 +0300

    Dashboard blocks (sms send)

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -5,6 +5,8 @@ namespace Ai\AdminBundle\Controller;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Validator\Constraints\Collection;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\AdminBundle\Exception\ModelManagerException;
 
 class AdminController extends CRUDController
 {
@@ -36,5 +38,26 @@ class AdminController extends CRUDController
         $this->get('session')->getFlashBag()->set('sonata_flash_info', $translator->trans('Position updated'));
 
         return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    public function batchActionSendSms(ProxyQueryInterface $query)
+    {
+        if (false === $this->admin->isGranted('SEND_SMS')) {
+            throw new AccessDeniedException();
+        }
+
+        $translator = $this->get('translator');
+
+        if ( !$smsText = $this->get('request')->get('sms_text') )
+        {
+            $this->get('session')->getFlashBag()->set('sonata_flash_error', $translator->trans('Sms text is empty!'));
+        } elseif ( count($this->get('request')->get('idx')) == 0 )
+        {
+            $this->get('session')->getFlashBag()->set('sonata_flash_error', $translator->trans('Elements is not select!'));
+        } else {
+            $this->get('session')->getFlashBag()->set('sonata_flash_success', $translator->trans('SMS send successful!'));
+        }
+
+        return $this->redirectToRoute('sonata_admin_dashboard');
     }
 }
