515ea6dfe1744c430793ab614c00d1bc58beb6c3
Ruslan <ruslana.net@gmail.com>
2015-11-12 01:56:21 +0300

    update structure

--- AiAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ai\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AiAdminBundle extends Bundle
-{
-}
--- Resources/public/js/admin.js
+++
@@ -1,7 +0,0 @@
-jQuery('#sonata-ba-field-container-{{ id }}').on('sonata.add_element', function(){
-    jQuery('#sonata-ba-field-container-{{ id }}').find('textarea.ckeditor').ckeditor();
-    <!--jQuery('#sonata-ba-field-container-{{ id }}').find('input.slider_ui').slider();-->
-    jQuery.datepicker.setDefaults( jQuery.datepicker.regional[ "" ] );
-    jQuery(".datepicker").datepicker( jQuery.datepicker.regional[ "ru" ]);
-    jQuery(".datepicker").hide();
-});
