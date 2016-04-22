5a213bbca9a8aee78a9cdd469884c1f74b3b7c9f
Ruslan Muriev <ruslana.net@gmail.com>
2016-04-22 22:25:09 +0300

    Add default mappers

--- src/Admin/DefaultAdmin.php
+++ src/Admin/DefaultAdmin.php
@@ -3,6 +3,10 @@
 namespace Ai\AdminBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Knp\Menu\ItemInterface as MenuItemInterface;
@@ -26,6 +30,264 @@ class DefaultAdmin extends Admin {
     protected $ExportFields=array();
 
     protected $translationDomain = 'messages';
+
+    protected $configureListWithourActions=false;
+
+    protected static $classTraites=[];
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('id');
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameTrait')){
+            $datagridMapper->add('name');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\DescrTrait')){
+            $datagridMapper->add('descr');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\ContentTrait')){
+            $datagridMapper->add('content');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CrdateTrait')){
+            $datagridMapper->add('crdate', 'doctrine_orm_date_range', array(), 'sonata_type_date_range',
+                array(
+                    'format' => 'dd-MM-yyyy',
+                    'widget' => 'single_text',
+                    'attr' => array('class' => 'datepicker'))
+            );
+        }
+        if($this->hasTrait('Ai\AdminBundle\Model\TstampTrait')){
+            $datagridMapper->add('tstamp', 'doctrine_orm_date_range', array(), 'sonata_type_date_range',
+                array(
+                    'format' => 'dd-MM-yyyy',
+                    'widget' => 'single_text',
+                    'attr' => array('class' => 'datepicker'))
+            );
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\EnabledTrait')){
+            $datagridMapper->add('enabled');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CeoTrait')){
+            $datagridMapper
+                ->add('ceoTitle')
+                ->add('ceoKeywords')
+                ->add('ceoDescription')
+            ;
+        }
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->with('Default');
+
+        if($this->hasTrait('Ai\AdminBundle\Model\TitleTrait')){
+            $formMapper->add('title');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameTrait')){
+            $formMapper->add('name');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\DescrTrait')){
+            $formMapper->add('descr', null, array('attr'=>array('class'=>'ckeditor')));
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\ContentTrait')){
+            $formMapper->add('content', null, array('attr'=>array('class'=>'ckeditor')));
+        }
+
+        if($this->hasTrait('Ai\CmsCoreBundle\Model\FilesTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\CmsCoreBundle\Model\ImagesTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\FileTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\EnabledTrait')){
+            $formMapper->add('enabled');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\OnlyRegisteredTrait')){
+            $formMapper->add('onlyRegistered');
+        }
+
+        $formMapper->end();
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CeoTrait')){
+            $formMapper
+                ->with('CEO')
+                ->add('ceoTitle')
+                ->add('ceoKeywords')
+                ->add('ceoDescription')
+                ->end()
+            ;
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameSlugTrait')){
+            $formMapper->with('CEO')->add('slug')->end();
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\TitleSlugTrait')){
+            $formMapper->with('CEO')->add('slug')->end();
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CacheTrait')){
+            $formMapper
+                ->with('Other')
+                ->add('noCache')
+                ->add('cacheTimeout')
+                ->end();
+        }
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->add('id');
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameTrait')){
+            $listMapper->add('name');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\DescrTrait')){
+            $listMapper->add('descr');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\EnabledTrait')){
+            $listMapper->add('enabled', null, ['editable' => true]);
+        }
+        
+        if($this->configureListWithourActions === false){
+            self::configureListAction($listMapper);
+        }
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListAction(ListMapper $listMapper)
+    {
+        if(!$listMapper->has('_action')) {
+            $listMapper
+                ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'show' => array(),
+                        'edit' => array(),
+                        'delete' => array(),
+                        'move' => array(
+                            'template' => 'PixSortableBehaviorBundle:Default:_sort.html.twig'
+                        ),
+                    )
+                ));
+        }
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+
+        $showMapper->with('Default');
+
+        $showMapper->add('id');
+
+        if($this->hasTrait('Ai\AdminBundle\Model\TitleTrait')){
+            $showMapper->add('title');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameTrait')){
+            $showMapper->add('name');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\DescrTrait')){
+            $showMapper->add('descr', null, array('attr'=>array('class'=>'ckeditor')));
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\ContentTrait')){
+            $showMapper->add('content', null, array('attr'=>array('class'=>'ckeditor')));
+        }
+
+        if($this->hasTrait('Ai\CmsCoreBundle\Model\FilesTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\CmsCoreBundle\Model\ImagesTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\FileTrait')){
+            //TODO
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\EnabledTrait')){
+            $showMapper->add('enabled');
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\OnlyRegisteredTrait')){
+            $showMapper->add('onlyRegistered');
+        }
+
+        $showMapper->end();
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CeoTrait')){
+            $showMapper
+                ->with('CEO')
+                ->add('ceoTitle')
+                ->add('ceoKeywords')
+                ->add('ceoDescription')
+                ->end()
+            ;
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\NameSlugTrait')){
+            $showMapper->with('CEO')->add('slug')->end();
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\TitleSlugTrait')){
+            $showMapper->with('CEO')->add('slug')->end();
+        }
+
+        if($this->hasTrait('Ai\AdminBundle\Model\CacheTrait')){
+            $showMapper
+                ->with('Other')
+                ->add('noCache')
+                ->add('cacheTimeout')
+                ->add('crdate', 'datetime', array(
+                    'label' => 'Дата создания',
+                    'read_only' => true,
+                    'widget' => 'single_text',
+                    'format' => 'd.m.Y H:m',
+                    'required' => false,
+                ))
+                ->add('tstamp', 'datetime', array(
+                    'label' => 'Дата обновления',
+                    'read_only' => true,
+                    'widget' => 'single_text',
+                    'format' => 'd.m.Y H:m',
+                    'required' => false,
+                ))
+                ->end();
+        }
+    }
 
     /**
      * @param ContainerInterface $container
@@ -161,5 +423,30 @@ class DefaultAdmin extends Admin {
         }
 
         return $actions;
+    }
+
+    /**
+     * Check entity trait
+     *
+     * @param $traitClass
+     * @return mixed
+     */
+    public function hasTrait($traitClass)
+    {
+        return array_key_exists($traitClass, $this->getClassUses());
+    }
+
+    /**
+     * Get entity traites
+     *
+     * @return array
+     */
+    public function getClassUses()
+    {
+        if(!empty(self::$classTraites)){
+            return self::$classTraites;
+        }
+
+        return self::$classTraites = class_uses($this->getClass());
     }
 } 
